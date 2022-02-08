adbe10acfdce79c8696cd508b2ef83885664f517
Boris Novikov <boris.novikov95@gmail.com>
2022-02-08 13:40:12 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import Flickity from "react-flickity-component";
 
 function App() {
+  const ref = useRef<Flickity>(null);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box>
+          <Heading>Damn that error!</Heading>
+          <Flex as={Flickity} ref={ref}>
+            <Box>item1</Box>
+            <Box>item2</Box>
+            <Box>item3</Box>
+            <Box>item4</Box>
+            <Box>item5</Box>
+          </Flex>
+        </Box>
       </header>
     </div>
   );
